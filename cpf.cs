35b8d6aa4ba49333236bb8e99fc6ecc80a00b4ee
Luana Lúcia <92230804+LuanaLucia@users.noreply.github.com>
2022-04-29 11:02:11 -0300

    Update cpf.cs

--- cpf.cs
+++ cpf.cs
@@ -4,7 +4,19 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine(validaCPF("36698765484"));
+        Console.WriteLine(validaCPF("911.160.010-12"));
+        Console.WriteLine(validaCPF("282.852.80-35"));
+        Console.WriteLine(validaCPF("806840844064"));
+        Console.WriteLine(validaCPF("845.594.060-30 "));
+        Console.WriteLine(validaCPF("  282.852.810-35"));
+        Console.WriteLine(validaCPF("   0806.840.440-64"));
+        Console.WriteLine(validaCPF("455 566 380 20"));
+        Console.WriteLine(validaCPF("       28285281035"));
+        Console.WriteLine(validaCPF("25549038080"));
+        Console.WriteLine(validaCPF("00000000000"));
+        Console.WriteLine(validaCPF("84559406030"));
+
+
     }
 
     public static string validaCPF(string CPF)
@@ -19,17 +31,16 @@ public class Program
         int digito1 = calculaDigito(11, CPFtratado);
         int digito2 = calculaDigito(12, CPFtratado);
 
-        //return Convert.ToInt64(digito1).ToString("D1");
-
-        if (Convert.ToInt64(digito1).ToString("D1") == CPFtratado.Substring(9, 1) || Convert.ToInt64(digito2).ToString("D1") == CPFtratado.Substring(10, 1))
+        if (Convert.ToInt64(digito2).ToString("D1") == CPFtratado.Substring(10, 1) &&
+           Convert.ToInt64(digito1).ToString("D1") == CPFtratado.Substring(9, 1) &&
+           CPFtratado != "00000000000")
         {
-            return "CPF Válido";
+            return "CPF válido";
         }
         else
         {
             return "CPF inválido";
         }
-
 
     }
 
@@ -37,9 +48,9 @@ public class Program
     {
         int digitoAux = 0;
 
-        for (int i = 1; i < CPFaux.Length - 1; i++)
+        for (int i = 1; i < (n - 1); i++)
         {
-            digitoAux += (n - i) * Int32.Parse(CPFaux.Substring(i, 1));
+            digitoAux += (n - i) * Int32.Parse(CPFaux.Substring(i - 1, 1));
 
         }
 
